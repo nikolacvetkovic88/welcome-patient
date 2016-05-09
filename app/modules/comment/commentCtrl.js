543f1ffa80c87a1add360fe9e09d83e32ffc7042
Nikola Cvetkovic <Nikola Cvetkovic>
2016-05-09 21:38:43 +0100

    Added submission of new comments to the UI

--- app/modules/comment/commentCtrl.js
+++ app/modules/comment/commentCtrl.js
@@ -1,57 +1,75 @@
 app.controller('commentCtrl', function($scope, commentRepository) {
-  $scope.$emit('body:class:add', "transparent");
-  $scope.loading = true;
-  $scope.commentsToDisplay = null;
+	$scope.$emit('body:class:add', "transparent");
+	$scope.commentsToDisplay = null;
 
-  commentRepository.getComments()
-  .success(function(data) {
-    $scope.loading = false;
-    $scope.parseData(data);
-    $scope.setContent("today");
-  })
-  .error(function() {
-    $scope.loading = false;
-    bootbox.alert("<div class='text-danger'>Failed loading comments.</div>"); 
-  });
+    $scope.loadComments = function() {
+    	$scope.loading = true;
+		commentRepository.getComments()
+		.success(function(data) {
+			$scope.loading = false;
+			$scope.parseData(data);
+			$scope.setContent("today");
+		})
+		.error(function() {
+			$scope.loading = false;
+			bootbox.alert("<div class='text-danger'>Failed loading comments.</div>"); 
+		});
+	}
 
-  $scope.parseData = function(data) {
-  	var comments = [];
+	$scope.parseData = function(data) {
+		var comments = [];
 
-	angular.forEach(data.todayComments, function(comment, key) {
-    	comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
-    });
-    $scope.commentsToday = comments;
-    comments = [];
+		angular.forEach(data.todayComments, function(comment, key) {
+			comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
+		});
+		$scope.commentsToday = comments;
+		comments = [];
 
-    angular.forEach(data.weekComments, function(comment, key) {
-    	comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
-    });
-    $scope.commentsWeek = comments;
-    comments = [];
+		angular.forEach(data.weekComments, function(comment, key) {
+			comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
+		});
+		$scope.commentsWeek = comments;
+		comments = [];
 
-    angular.forEach(data.monthComments, function(comment, key) {
-    	comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
-    });
-    $scope.commentsMonth = comments;
-    comments = [];
-  }
+		angular.forEach(data.monthComments, function(comment, key) {
+			comments.push({ dateTime: comment.dateTime, sender: comment.sender, content: comment.content });
+		});
+		$scope.commentsMonth = comments;
+		comments = [];
+	}
 
-  $scope.setContent = function(mode) {
-    $scope.commentType = mode;
-    
-    switch  (mode) {
-      case  "today":
-        $scope.commentsToDisplay = $scope.commentsToday;
-        break;
-      case  "week":
-        $scope.commentsToDisplay = $scope.commentsWeek;
-        break;
-      case  "month":
-        $scope.commentsToDisplay = $scope.commentsMonth;
-        break;
-      default:
-            break;
-    }
-  }
+	$scope.setContent = function(mode) {
+		$scope.commentType = mode;
 
+		switch  (mode) {
+			case  "today":
+			    $scope.commentsToDisplay = $scope.commentsToday;
+			    break;
+			case  "week":
+			    $scope.commentsToDisplay = $scope.commentsWeek;
+			    break;
+			case "month":
+			    $scope.commentsToDisplay = $scope.commentsMonth;
+			    break;
+			case "new":
+			    $scope.myComment = "";
+			    $scope.commentsToDisplay = null;
+			default:
+			    break;
+		}
+	}
+
+	$scope.submitNewComment = function() {
+		if(!$scope.myComment || !$scope.myComment.length) {
+			bootbox.alert("<div class='text-danger'>Please type your comment!</div>");
+			return false;
+		} else {
+			setTimeout(function() {
+				bootbox.alert("<div class='text-info'>Your comment has been submitted successfully!");
+				$scope.loadComments();
+			}, 1000);
+		}
+	}
+
+	$scope.loadComments();
 });

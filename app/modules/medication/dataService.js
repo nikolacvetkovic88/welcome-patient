74cb83b05a7fc76f1d1451ad483f2ae4b0c73d0f
Nikola Cvetkovic <Nikola Cvetkovic>
2016-08-14 16:54:06 +0100

    Implement parsing Medication data in the Medication service, Chain promsies in the controller to get the data aka constuct RDF query requests

--- app/modules/medication/dataService.js
+++ app/modules/medication/dataService.js
@@ -1,28 +1,22 @@
-app.factory('medicationRepository', function($base64, $http) {
-var MedicationRepository = {},
-    baseUrl = 'http://aerospace.med.auth.gr:8080/welcome/api/data/';
+app.factory('medicationRepository', function($base64, $http, $q) {
+	var MedicationRepository = {},
+	baseUrl = 'http://aerospace.med.auth.gr:8080/welcome/api/data/';
 
-MedicationRepository.getMedications = function(username, password, patientId) {
-        /*var getMedicationPrescriptionData = function(url) {
-            return  $http({
-                url: url,
-                method: 'GET',
-                headers: {
-                    'Authorization' : 'Basic ' + encodedCred,
-                    'Accept' : 'text/turtle',
-                    'Content-Type' : 'text/turtle'
-                }
-            })
-            .success(function(data) {
-                console.log(data);
-            })
-            .error(function() {
-                $scope.loading = false;
-                bootbox.alert("<div class='text-danger'>Error while getting medications. If this continues please contact support.</div>");
-            });
-        }*/
+	MedicationRepository.getMedicationPrescriptions= function(username, password, patientId) {
+		var url =  baseUrl + 'Patient/' + patientId + '/MedicationPrescription';//'http://aerospace.med.auth.gr:8080/welcome/api/data/MedicationPrescription/99425607-9d7a-497f-aeda-c7a3e866d381';
+		var encodedCred = $base64.encode(username + ':' + password);
+		return  $http({
+			url: url,
+			method: 'GET',
+			headers: {
+				'Authorization' : 'Basic ' + encodedCred,
+				'Accept' : 'text/turtle',
+				'Content-Type' : 'text/turtle'
+			}
+		});
+    }
 
-        var url =  baseUrl + 'Patient/' + patientId + '/MedicationPrescription';//'http://aerospace.med.auth.gr:8080/welcome/api/data/MedicationPrescription/99425607-9d7a-497f-aeda-c7a3e866d381';
+    MedicationRepository.getMedicationPrescriptionOrMedicationByRef = function(username, password, url) {
         var encodedCred = $base64.encode(username + ':' + password);
         return  $http({
             url: url,
@@ -32,22 +26,81 @@ MedicationRepository.getMedications = function(username, password, patientId) {
                 'Accept' : 'text/turtle',
                 'Content-Type' : 'text/turtle'
             }
-        })
-        .success(function(data) {
-            console.log(data);
+        });
+    }
 
-            var parser = N3.Parser();
-            var N3Util = N3.Util;
-            parser.parse(data, function(error, triple) {
+    MedicationRepository.decodeMedicationPrescriptions = function(data, patientId) {
+    	var subject = "http://aerospace.med.auth.gr:8080/welcome/api/data/Patient/" + patientId + "/MedicationPrescription";
+    	var predicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+    	var parser = N3.Parser();
+        var N3Util = N3.Util;
+        var prescriptionRefs = [];
+        var defer = $q.defer();
+
+        parser.parse(data, function(error, triple) {
+            if(triple) {
+                if (triple && triple.subject === subject && triple.predicate != predicate) {
+                    prescriptionRefs.push(triple.object);
+                } 
+            }else if (error) {
+            // Check for errors here and possibly reject the promise
+            } else {
+                    // When the function execution reaches this, it signals that all triples are successfully parsed
+                    // and you can resolve the promise here/
+                    defer.resolve(prescriptionRefs);
+            }
+        });
+		
+		return defer.promise;
+    }
+
+    MedicationRepository.decodeMedicationPrescription = function(data) {
+    	var parser = N3.Parser({ format: 'application/turtle' });
+        var N3Util = N3.Util;
+        var medicationRefs = [];
+        var defer = $q.defer();
+
+        parser.parse(data,
+            function (error, triple) {
                 if (triple) {
-                    if (N3Util.isLiteral(triple.object)) {
-                        console.log(triple.predicate); 
-                        console.log(triple.subject);
+                    if(!N3Util.isBlank(triple.subject) && triple.predicate === 'http://lomi.med.auth.gr/ontologies/FHIRResources#medication') {
+                        medicationRefs.push(triple.object);
                     }
+					
+					/*if(!N3Util.isBlank(triple.subject) && triple.predicate === 'http://lomi.med.auth.gr/ontologies/FHIRResources#patient') {
+                        var strArray = triple.object.split('/');
+                        medObj.patientUri = strArray[strArray.length-1];
+                        return;
+                    }*/
+                } else if (error) {
+
+                } else {
+                    defer.resolve(medicationRefs);
                 }
             });
-        });
-    };
+
+        return defer.promise;
+    }
+
+    MedicationRepository.decodeMedication = function(data) {
+        var parser = N3.Parser({ format: 'application/turtle' });
+        var N3Util = N3.Util;
+        var medObj = {};
+        var defer = $q.defer();
+
+        parser.parse(data,
+            function (error, triple) {
+                if (triple) {
+                    if(N3Util.isBlank(triple.subject) && triple.predicate === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value' && N3Util.isLiteral(triple.object) ) {//http://lomi.med.auth.gr/ontologies/FHIRResources#Medication.name
+                        medObj.medication = N3Util.getLiteralValue(triple.object);
+                    }
+                } else {
+                    defer.resolve(medObj);
+                }
+            });
+
+        return defer.promise;
+    }
 
     return MedicationRepository;
 });
--- app/modules/medication/medicationCtrl.js
+++ app/modules/medication/medicationCtrl.js
@@ -1,18 +1,66 @@
-app.controller('medicationCtrl', function($scope, $rootScope, medicationRepository) {
+app.controller('medicationCtrl', function($scope, $rootScope, $q, medicationRepository) {
 	$scope.$emit('body:class:add', "transparent");
-	$scope.loading = true;
-	$scope.medications = null;
 	$scope.patientId = $rootScope.Patient ? $rootScope.Patient.cloudRef : null;
 
 	$scope.getMedications = function() {
-		medicationRepository.getMedications('welk','welk', $scope.patientId)
+		/*medicationRepository.getMedicationPrescriptions('welk','welk', $scope.patientId)
 		.success(function(data) {
+			$scope.medications = medicationRepository.decodeMedicationPrescriptions('welk', 'welk', $scope.patientId, data);
 			$scope.loading = false;
 		})
 		.error(function() {
             $scope.loading = false;
             bootbox.alert("<div class='text-danger'>Error while getting medications. If this continues please contact support.</div>");
+        });*/
+        $scope.loading = true;
+        medicationRepository.getMedicationPrescriptions('welk', 'welk', $scope.patientId)
+        .then(function(response) {
+        	return medicationRepository.decodeMedicationPrescriptions(response.data, $scope.patientId);
+        })
+        .then(function(prescriptionRefs) {
+        	return $scope.getMedicationPrescriptionOrMedicationUriPromises(prescriptionRefs);
+        })
+        .then(function(prescriptions) {
+        	return $scope.getAllMedicationPrescriptions(prescriptions);
+        })
+        .then(function(medicationRefs) {
+        	return $scope.getMedicationPrescriptionOrMedicationUriPromises(medicationRefs);
+        })
+        .then(function(medications) {
+        	console.log(medications);
+        	return $scope.getAllMedications(medications);
+        })
+        .then(function(results) {
+        	$scope.medications = results;
+        	$scope.loading = false;
         });
+	}
+
+	$scope.getMedicationPrescriptionOrMedicationUriPromises = function(refs) {	
+        var promises = [];
+        angular.forEach(refs, function(ref) {
+            promises.push(medicationRepository.getMedicationPrescriptionOrMedicationByRef('welk', 'welk', ref));
+        });
+
+        return $q.all(promises);
+	}
+
+	$scope.getAllMedicationPrescriptions = function(prescriptions) {
+		var promises = [];
+        angular.forEach(prescriptions, function(prescription) {
+            promises.push(medicationRepository.decodeMedicationPrescription(prescription.data));
+        });
+
+        return $q.all(promises);
+	}
+
+	$scope.getAllMedications = function(medications) {
+		var promises = [];
+        angular.forEach(medications, function(medication) {
+            promises.push(medicationRepository.decodeMedication(medication.data));
+        });
+
+        return $q.all(promises);
 	}
 
 	$scope.getMedications();   

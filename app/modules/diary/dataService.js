63ae550e0a0f321ba90e67fa13e8adeac0f33cc5
Nikola Cvetkovic <nikolacvetkovic88@hotmail.com>
2016-10-09 21:34:16 +0100

    Fully implemented getting measurements and questionnaires diary data

--- app/modules/diary/dataService.js
+++ app/modules/diary/dataService.js
@@ -1,14 +1,225 @@
-app.factory('diaryRepository', function($http) {
+app.factory('diaryRepository', function($base64, $http, $q) {
+	var DiaryRepository = {},
+	baseUrl = 'http://aerospace.med.auth.gr:8080/welcome/api/data/';
 
-var diaryUrl = "app/modules/testData/diary.json";
+    DiaryRepository.getQuestionnaireDiaryEntries = function(username, password, patientId) {
+    	var url =  baseUrl + 'Patient/' + patientId + '/QuestionnaireOrder';
+		var encodedCred = $base64.encode(username + ':' + password);
+		return $http({
+			url: url,
+			method: 'GET',
+			headers: {
+				'Authorization' : 'Basic ' + encodedCred,
+				'Accept' : 'text/turtle',
+				'Content-Type' : 'text/turtle'
+			}
+		});
+    }
 
-return {
-        getAllDiaryEntries: function() {
-            return $http( {
-                method: 'GET',
-                url: diaryUrl
-            })
-        }
-    };
+    DiaryRepository.decodeQuestionnaireDiaryEntries = function(data, patientId) {
+    	var subject = "http://aerospace.med.auth.gr:8080/welcome/api/data/Patient/" + patientId + "/QuestionnaireOrder";
+    	var predicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+    	var parser = N3.Parser();
+        var N3Util = N3.Util;
+        var diaryQuestionnaireRefs = [];
+        var defer = $q.defer();
 
+        parser.parse(data, function(error, triple) {
+            if(triple) {
+                if (triple && triple.subject === subject && triple.predicate != predicate) {
+                    diaryQuestionnaireRefs.push(triple.object);
+                } 
+            }else if (error) {
+            // Check for errors here and possibly reject the promise
+            } else {
+                    // When the function execution reaches this, it signals that all triples are successfully parsed
+                    // and you can resolve the promise here/
+                    defer.resolve(diaryQuestionnaireRefs);
+            }
+        });
+		
+		return defer.promise;
+    }
+
+    DiaryRepository.getQuestionnaireDiaryEntryByRef = function(username, password, url) {
+    	var encodedCred = $base64.encode(username + ':' + password);
+        return  $http({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Basic ' + encodedCred,
+                'Accept' : 'text/turtle',
+                'Content-Type' : 'text/turtle'
+            }
+        });
+    }
+
+    DiaryRepository.decodeQuestionnaireDiaryEntry = function(data) {
+    	var parser = N3.Parser({ format: 'application/turtle' });
+        var N3Util = N3.Util;
+        var questionnaireDiaryObj = {
+        	eventDates: []
+        };
+        var defer = $q.defer();
+        var isFirst = true;
+
+        parser.parse(data,
+            function (error, triple) {
+                if (triple) {
+                     /*console.log("triple has been found: ");
+                     console.log("Subject: ", triple.subject, '.',"Blank? ", N3Util.isBlank(triple.subject),
+                            "IRI? ", N3Util.isIRI(triple.subject),"Literal? ", N3Util.isLiteral(triple.subject));
+                     console.log("Predicate: ", triple.predicate);
+                     console.log("Object", triple.object, '.',"Blank? ", N3Util.isBlank(triple.object),
+                            "IRI? ", N3Util.isIRI(triple.object),"Literal? ", N3Util.isLiteral(triple.object));
+                     console.log("*****************************************************************");*/
+
+                    if(N3Util.isBlank(triple.subject) && triple.predicate === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value' && N3Util.isLiteral(triple.object) ) {
+                        if(N3Util.getLiteralType(triple.object) === "http://www.w3.org/2001/XMLSchema#dateTime") {
+                        	if(!isFirst)
+                        		questionnaireDiaryObj.eventDates.push(N3Util.getLiteralValue(triple.object));
+        	                isFirst = false;
+                        }
+                    }
+
+                    if(triple.predicate == "http://lomi.med.auth.gr/ontologies/FHIRResources#detail" && N3.Util.isIRI(triple.object)) {
+                        questionnaireDiaryObj.title = triple.object.split('#')[1].split('_')[1];
+                        return;
+                    }
+
+                }else if (error) {
+                // Check for errors here and possibly reject the promise 
+                }else {
+                    defer.resolve(questionnaireDiaryObj);
+                }
+            });
+
+        return defer.promise;
+    }
+
+    DiaryRepository.getDevices = function(username, password, patientId) {
+        var url =  baseUrl + 'Patient/' + patientId + '/PortableBiomedicalSensorDevice';
+        var encodedCred = $base64.encode(username + ':' + password);
+        return $http({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Basic ' + encodedCred,
+                'Accept' : 'text/turtle',
+                'Content-Type' : 'text/turtle'
+            }
+        });
+    }
+
+    DiaryRepository.decodeDevices  = function(data, patientId) {
+        var subject = "http://aerospace.med.auth.gr:8080/welcome/api/data/Patient/" + patientId + "/PortableBiomedicalSensorDevice";
+        var predicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+        var parser = N3.Parser();
+        var N3Util = N3.Util;
+        var deviceRefs = [];
+        var defer = $q.defer();
+
+        parser.parse(data, function(error, triple) {
+            if(triple) {
+                if (triple && triple.subject === subject && triple.predicate != predicate) {
+                    deviceRefs.push(triple.object);
+                } 
+            }else if (error) {
+            // Check for errors here and possibly reject the promise
+            } else {
+                    // When the function execution reaches this, it signals that all triples are successfully parsed
+                    // and you can resolve the promise here/
+                    defer.resolve(deviceRefs);
+            }
+        });
+        
+        return defer.promise;
+    }
+
+    DiaryRepository.getDeviceByRef = function(username, password, url) {
+        var encodedCred = $base64.encode(username + ':' + password);
+        return  $http({
+            url: url + "/DeviceUseRequest",
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Basic ' + encodedCred,
+                'Accept' : 'text/turtle',
+                'Content-Type' : 'text/turtle'
+            }
+        });
+    }
+
+    DiaryRepository.decodeDeviceEntry = function(data) {
+        var partSubject = "http://aerospace.med.auth.gr:8080/welcome/api/data/PortableBiomedicalSensorDevice/";
+        var predicate = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+        var parser = N3.Parser();
+        var N3Util = N3.Util;
+        var deviceEntryRefs = [];
+        var defer = $q.defer();
+
+        parser.parse(data, function(error, triple) {
+            if(triple) {              
+                if (!N3.Util.isBlank(triple.subject) && triple.subject.indexOf(partSubject) != -1 && triple.subject.indexOf("/DeviceUseRequest") != -1 && triple.predicate != predicate) {
+                     deviceEntryRefs.push(triple.object);
+                }
+            }else if (error) {
+            // Check for errors here and possibly reject the promise
+            } else {
+                    // When the function execution reaches this, it signals that all triples are successfully parsed
+                    // and you can resolve the promise here/
+                    defer.resolve(deviceEntryRefs);
+            }
+        });
+        
+        return defer.promise;
+    }
+
+
+    DiaryRepository.getDeviceDiaryEntryByRef = function(username, password, url) {
+        var encodedCred = $base64.encode(username + ':' + password);
+        return  $http({
+            url: url + "?depth=2&version=v2",
+            method: 'GET',
+            headers: {
+                'Authorization' : 'Basic ' + encodedCred,
+                'Accept' : 'text/turtle',
+                'Content-Type' : 'text/turtle'
+            }
+        });
+    }
+
+    DiaryRepository.decodeDeviceDiaryEntry = function(data) {
+        var parser = N3.Parser({ format: 'application/turtle' });
+        var N3Util = N3.Util;
+        var deviceDiaryObj = {
+            eventDates: []
+        };
+        var defer = $q.defer();
+        var isFirst = true;
+
+        parser.parse(data,
+            function (error, triple) {
+                if (triple) {
+                    if(N3Util.isBlank(triple.subject) && triple.predicate === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#value' && N3Util.isLiteral(triple.object) ) {
+                        if(N3Util.getLiteralType(triple.object) === "http://www.w3.org/2001/XMLSchema#dateTime") {
+                            if(!isFirst)
+                               deviceDiaryObj.eventDates.push(N3Util.getLiteralValue(triple.object));
+                            isFirst = false;
+                        }
+                        if(N3Util.getLiteralType(triple.object) === "http://www.w3.org/2001/XMLSchema#string") {
+                            deviceDiaryObj.title = N3Util.getLiteralValue(triple.object);
+                        }
+                    }
+                }else if (error) {
+                // Check for errors here and possibly reject the promise 
+                }else {
+                    defer.resolve(deviceDiaryObj);
+                }
+            });
+
+        return defer.promise;
+    }
+
+
+    return DiaryRepository;
 });
--- app/modules/diary/diaryCtrl.js
+++ app/modules/diary/diaryCtrl.js
@@ -1,98 +1,155 @@
-app.controller('diaryCtrl', function($scope, $window, diaryRepository) {
+app.controller('diaryCtrl', function($scope, $rootScope, $window, $q, diaryRepository) {
   $scope.$emit('body:class:add', "transparent");
-  $scope.loading = true;
-  $scope.events = [
-  ];
+  $scope.patientId = $rootScope.Patient ? $rootScope.Patient.cloudRef : null;
   $scope.eventSources = [];
-  $scope.diaryData = null;
+  $scope.diaryToday = [];
+  $scope.diaryTomorrow = [];
 
-  $scope.getDiaryEntries = function() {
-    diaryRepository.getAllDiaryEntries()
-    .success(function(data) {
-      $scope.loading = false;
-      $scope.diaryData = data;
-      $scope.addEvents();
+  $scope.getQuestionnaireDiaryEntries = function(_callback) {
+    $scope.loading = true;
+
+    diaryRepository.getQuestionnaireDiaryEntries('welk', 'welk', $scope.patientId)
+    .then(function(response) {
+      return diaryRepository.decodeQuestionnaireDiaryEntries(response.data, $scope.patientId); 
     })
-    .error(function() {
+    .then(function(questionnaireDiaryRefs) {
+      return $scope.getQuestionnaireDiaryEntryUriPromises(questionnaireDiaryRefs); 
+    })
+    .then(function(questionnaireDiaries) {
+      return $scope.getAllQuestionnaireDiaryEntries(questionnaireDiaries);
+    })
+    .then(function(results) {
+      $scope.questionnaires = $scope.parseData(results, "q");
+      $scope.addEvents($scope.questionnaires);
+      $scope.diaryEntriesTodayAndTomorrow($scope.questionnaires);
       $scope.loading = false;
-      bootbox.alert("<div class='text-danger'>Failed loading diary entries.</div>"); 
+      _callback();
     });
   }
 
-  $scope.appointments = function() {
-    return $scope.parseData($scope.diaryData.appointments, "a");
+  $scope.getQuestionnaireDiaryEntryUriPromises = function(refs) {
+    var promises = [];
+    angular.forEach(refs, function(ref) {
+        promises.push(diaryRepository.getQuestionnaireDiaryEntryByRef('welk', 'welk', ref));
+    });
+
+    return $q.all(promises);
   }
 
-  $scope.measurements = function() {
-    return $scope.parseData($scope.diaryData.measurements, "m");
+  $scope.getAllQuestionnaireDiaryEntries = function(questionnaireDiaries) {
+    var promises = [];
+    angular.forEach(questionnaireDiaries, function(questionnaireDiary) {
+        promises.push(diaryRepository.decodeQuestionnaireDiaryEntry(questionnaireDiary.data));
+    });
+
+    return $q.all(promises);
   }
 
-  $scope.questionnaires = function() {
-    return $scope.parseData($scope.diaryData.questionnaires, "q");
+  $scope.getDevices = function() {
+    $scope.loading = true;
+
+    diaryRepository.getDevices('welk', 'welk', $scope.patientId)
+    .then(function(response) {
+      return diaryRepository.decodeDevices(response.data, $scope.patientId)
+    })
+    .then(function(deviceRefs) {
+      return $scope.getDeviceUriPromises(deviceRefs);
+    })
+    .then(function(response) {
+      return $scope.decodeDeviceEntries(response);
+    })
+    .then(function(deviceEntryRefs) {
+      var nonEmptyRefs = [];
+      angular.forEach(deviceEntryRefs, function(refArray) {
+        angular.forEach(refArray, function(ref) {
+          nonEmptyRefs.push(ref);
+        });
+      })
+
+      return $scope.getDeviceEntryUriPromises(nonEmptyRefs);
+    })
+    .then(function(deviceDiaries) {
+      return $scope.getAllDeviceDiaryEntries(deviceDiaries);
+    })
+    .then(function(results) {
+      $scope.devices = $scope.parseData(results, "m");
+      $scope.addEvents($scope.devices);
+      $scope.diaryEntriesTodayAndTomorrow($scope.devices);
+      $scope.loading = false;
+    });
+  }
+
+  $scope.getDeviceUriPromises = function(refs) {
+    var promises = [];
+    angular.forEach(refs, function(ref) {
+        promises.push(diaryRepository.getDeviceByRef('welk', 'welk', ref));
+    });
+
+    return $q.all(promises);
+  }
+
+  $scope.decodeDeviceEntries = function(deviceRefs) {
+    var promises = [];
+    angular.forEach(deviceRefs, function(ref) {
+        promises.push(diaryRepository.decodeDeviceEntry(ref.data));
+    });
+
+    return $q.all(promises);
+  }
+
+  $scope.getDeviceEntryUriPromises = function(refs) {
+    var promises = [];
+    angular.forEach(refs, function(ref) {
+        promises.push(diaryRepository.getDeviceDiaryEntryByRef('welk', 'welk', ref));
+    });
+
+    return $q.all(promises);
+  }
+
+  $scope.getAllDeviceDiaryEntries = function(deviceDiaries) {
+    var promises = [];
+    angular.forEach(deviceDiaries, function(deviceDiary) {
+        promises.push(diaryRepository.decodeDeviceDiaryEntry(deviceDiary.data));
+    });
+
+    return $q.all(promises);
   }
 
   $scope.parseData = function(data, mode) {
     var parsedData = [];
 
-    angular.forEach(data,function(value, key){
-      var parsedObject = {};
-      parsedObject.title = value.name;
-      parsedObject.start = value.dateTime;
-      parsedObject.mode = mode;
-      parsedData.push(parsedObject);
+    angular.forEach(data, function(value) {
+      angular.forEach(value.eventDates, function(date) {
+        var parsedObject = {};
+        parsedObject.title = value.title;
+        parsedObject.start = moment(date, "YYYY-MM-DD HH:mm");
+        parsedObject.mode = mode;
+        parsedData.push(parsedObject);
+      });
     });
 
     return parsedData;
   }
 
-  $scope.diaryEntries = function(compareToDate) {
-    var diaryData = [];
+  $scope.diaryEntriesTodayAndTomorrow = function(data) {
+    $scope.diaryEntriesForDay($scope.diaryToday, data, moment());
+    $scope.diaryEntriesForDay($scope.diaryTomorrow, data, moment().add(1, 'days'));
+  }
 
-    angular.forEach($scope.appointments(), function(value, key) {
-      var appointmentDate = moment(value.start, 'YYYY-MM-DD');
-
-      if(compareToDate.isSame(appointmentDate, 'year') && compareToDate.isSame(appointmentDate, 'month') && compareToDate.isSame(appointmentDate, 'day')){
-        diaryData.push({mode: value.mode, title: value.title, start: formatDateForUser(value.start)});
+  $scope.diaryEntriesForDay = function(context, data, compareToDate) {
+    angular.forEach(data, function(value, key) {
+      var date = moment(value.start, 'YYYY-MM-DD');
+      if(compareToDate.isSame(date, 'year') && compareToDate.isSame(date, 'month') && compareToDate.isSame(date, 'day')){
+        context.push({mode: value.mode, title: value.title, start: formatDateForUser(value.start)});
       }
     });
-    angular.forEach($scope.measurements(), function(value, key) {
-      var measurementDate = moment(value.start, 'YYYY-MM-DD');
-
-      if(compareToDate.isSame(measurementDate, 'year') && compareToDate.isSame(measurementDate, 'month') && compareToDate.isSame(measurementDate, 'day')){
-        diaryData.push({mode: value.mode, title: value.title, start: formatDateForUser(value.start)});
-      }
-    });
-    angular.forEach($scope.questionnaires(), function(value, key) {
-      var questionnaireDate = moment(value.start, 'YYYY-MM-DD');
-
-      if(compareToDate.isSame(questionnaireDate, 'year') && compareToDate.isSame(questionnaireDate, 'month') && compareToDate.isSame(questionnaireDate, 'day')){
-        diaryData.push({mode: value.mode, title: value.title, start: formatDateForUser(value.start)});
-      }
-    });
-
-    return diaryData;
   }
 
-  $scope.diaryEntriesToday = function() {
-    var today = moment();
-
-    return $scope.diaryEntries(today);
+  $scope.addEvents = function(data) {
+    $scope.eventSources.push(data);
   }
 
-  $scope.diaryEntriesTomorrow = function() {
-    var tomorrow = moment().add(1, 'days');
-
-    return $scope.diaryEntries(tomorrow);
-  }
-
-  $scope.addEvents = function() {
-    $scope.events = $scope.appointments().concat($scope.questionnaires(), $scope.measurements());
-    $scope.eventSources.push($scope.events);
-    $scope.diaryToday = $scope.diaryEntriesToday();
-    $scope.diaryTomorrow = $scope.diaryEntriesTomorrow();
-  }
-
-  $scope.onClick = function( date, jsEvent, view) {
+  $scope.onClick = function(date, jsEvent, view) {
     var mode = date.mode,
     location = null;
 
@@ -147,7 +204,7 @@ app.controller('diaryCtrl', function($scope, $window, diaryRepository) {
     }
   }
 
-  /* config object */
+  /* calendar config object */
   $scope.uiConfig = {
     calendar:{
       height: "auto",
@@ -166,6 +223,21 @@ app.controller('diaryCtrl', function($scope, $window, diaryRepository) {
       }
     }    
   };
+
+  $scope.resetData = function() {
+    $scope.eventSources.length = 0;
+    $scope.diaryToday.length = 0;
+    $scope.diaryTomorrow.length = 0;
+  }
+
+  $scope.refresh = function() {
+    $scope.getDiaryEntries();
+  }
+ 
+  $scope.getDiaryEntries = function() {
+    $scope.resetData();
+    $scope.getQuestionnaireDiaryEntries($scope.getDevices);
+  }
 
   $scope.getDiaryEntries();
 });
